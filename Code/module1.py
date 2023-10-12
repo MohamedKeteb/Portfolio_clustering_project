8cc401450faf279aedc897157bf25271cf7637f8
NailKhelifa <nail.khelifa@ensae.fr>
2023-10-12 17:59:36 +0200

    Nail

--- Code/module1.py
+++ Code/module1.py
@@ -1,11 +1,12 @@
 import pandas as pd 
 import yfinance as yf
+import plotly.express as px 
 
 def get_returns(start_date, end_date, ticker_list): 
-    '''
     
+    '''
     ----------------------------------------------------------------
-    PURPOSE : create a dataframe of returns for a given period of time 
+    GENERAL IDEA : create a dataframe of returns for a given period of time 
               in [start, end] for a list of tickers
     ----------------------------------------------------------------
 
@@ -22,6 +23,68 @@ def get_returns(start_date, end_date, ticker_list):
     '''
 
     df = pd.DataFrame(yf.download(ticker_list, start_date, end_date))
-    data = (df['close'] - df['open'])/df['open']
-    data = data.dropna()
+    data = df['Adj Close'].pct_change()
     return data
+
+def plot_stock_return(data): 
+
+    
+    '''
+    ----------------------------------------------------------------
+    GENERAL IDEA : plot the returns of different stock on the same 
+                   time frame 
+    ----------------------------------------------------------------
+
+    ----------------------------------------------------------------
+    PARAMS : 
+    
+    - data : Pandas DataFrame object containing the returns of different
+             stocks for a given period of time
+
+    ----------------------------------------------------------------
+    '''
+    
+
+    ticker_list = data.columns.to_list()
+    fig = px.line(data, data.index, [data[ticker] for ticker in ticker_list], title='Stock returns')
+    fig.update_layout(title="Stock returns", legend_title="Stocks")
+    fig.update_yaxes(title_text="Returns")
+    fig.show()
+
+
+def multiple_clusterings(n_repeat, data, model):
+   '''
+   ----------------------------------------------------------------
+    ## Type of data ##
+
+    n_repeat : integer --> number of time we apply the clustering method
+    model : sklearn model we use --> e.g. GaussianMixture()
+    data : pd.DataFrame --> data we want to fit to the model
+
+    -------------
+
+    ## Output ##
+
+    Outputs a pandas DataFrame object of shape (len(data.index), n_repeat)
+
+    -------------
+
+    ## Genera idea ##
+
+    The idea is here to train the model on the dataset data multiple time (here n_repeat time)
+    and create a DataFrame whose columns are the cluster labels of each stock and whose rows are
+    the label of a given stock for each clustering method
+
+    '''
+   
+    Y = pd.DataFrame(index=data.index)
+    
+    for i in range(n_repeat):
+        model.fit(data)
+        predicted_labels = model.predict(data)
+        data_with_clusters = pd.DataFrame(predicted_labels, index=data.index)
+        y_i = "Clustering n°%i" % (i+1)
+        Y[y_i] = data_with_clusters
+
+    return Y
+    
