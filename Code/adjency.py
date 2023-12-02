1222337081fbeb04c8418b2b47dc18645d7e3452
NailKhelifa <nail.khelifa@ensae.fr>
2023-12-02 21:53:48 +0100

    adjency

--- Code/adjency.py
+++ Code/adjency.py
@@ -13,8 +13,58 @@ class Adjency:
     data : pandas DataFrame/List [x_1, ..., x_n]
     
     '''
+
     def __init__(self, data):
         self.data = data
+
+    def clean_data(self):
+
+        """
+        The columns of the dataframe are the following :
+
+        - "open"
+        - "high"
+        - "low"
+        - "close"
+        - "volume"
+        - "OPCL"
+        - "pvCLCL"
+        - "prevAdjClose
+        - "SPpvCLCL"
+        - "sharesOut"
+        - "PERMNO"
+        - "SICCD"
+        - "PERMCO"
+        - "prevRawOpen"
+        - "prevRawClose"
+        - "prevAdjOpen"
+
+        We first focus only on the three following : 
+        - "open"
+        - "close"
+        - "volume"
+        """
+
+        data = self.data[['ticker', 'open', 'close', 'volume']]
+
+        list = data['ticker']
+        data = data.drop(data.columns[0], axis=1)
+        data.set_index(list)
+
+        ## we now compute the returns 
+        
+        n = self.data.shape[0]
+
+        df2 = pd.DataFrame(index=data.index, columns=['return', 'volume'])
+        df2['volume'] = data['volume'] # volume
+        for i in range(n):
+            x = np.array(data.iloc[i][0].replace('[', '').replace(']', '').split(', '), dtype=float)## open
+            y = np.array(data.iloc[i][1].replace('[', '').replace(']', '').split(', '), dtype=float) ## close
+            z = (y - x) / x
+            df2.iloc[i, 0] = str(z)
+
+        self.data = df2 
+
 
     ## We first define distance matrix  
 
@@ -54,5 +104,47 @@ class Adjency:
                     y = self.data.iloc[j, 0]
                     A[i, j] = np.corrcoef(x, y)[0, 1]
 
+
+        return A
+    
+    def pearson_corr_volume(self, type='volume'):
+
+        """
+        a popular similarity measure in the literature is given by the Pearson correlation
+        coefficient that measures linear dependence between variables and takes values in 
+        [−1, 1]. By interpreting the correlation matrix as a weighted network whose (signed) 
+        edge weights capture the pairwise correlations, we cluster the multivariate time series by
+        clustering the underlying signed network
+        """
+        
+        n_stocks = self.data.shape[0]
+
+        A = np.zeros((n_stocks, n_stocks))
+
+        if type == 'volume': 
+            for i in range(n_stocks):
+
+                for j in range(i, n_stocks):
+
+                    if i == j: 
+                        A[i, j] = 1
+
+                    else: 
+                        x = np.array(self.data.iloc[i,2].replace('[', '').replace(']', '').split(', '), dtype=float)
+                        y = np.array(self.data.iloc[j,2].replace('[', '').replace(']', '').split(', '), dtype=float)
+                        A[i, j] = np.corrcoef(x, y)[0, 1]
+
+        if type == 'returns':
+            for i in range(n_stocks):
+
+                for j in range(i, n_stocks):
+
+                    if i == j: 
+                        A[i, j] = 1
+
+                    else: 
+                        x = np.array(self.data.iloc[i,0].replace('[', '').replace(']', '').split(', '), dtype=float)
+                        y = np.array(self.data.iloc[j,0].replace('[', '').replace(']', '').split(', '), dtype=float)
+                        A[i, j] = np.corrcoef(x, y)[0, 1]
 
         return A
