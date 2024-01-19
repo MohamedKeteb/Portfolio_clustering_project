86f571bfcc837eb46203b6a7e724bb400bfe4d37
NailKhelifa <nail.khelifa@ensae.fr>
2024-01-19 19:04:55 +0100

    process.py + january.ipynb

---
+++ Code/process.py
@@ -0,0 +1,101 @@
+import numpy as np
+import pandas as pd
+import ast
+import sys
+
+## path Nail : '/Users/khelifanail/Documents/GitHub/Portfolio_clustering_project'
+## path Jerome : 'C:/Users/33640/OneDrive/Documents/GitHub/Portfolio_clustering_project'
+sys.path.append(r'/Users/khelifanail/Documents/GitHub/Portfolio_clustering_project')  # Ajoute le chemin parent
+
+from signet.cluster import Cluster 
+from scipy import sparse
+
+
+# Function to safely convert a string into a list
+def safe_literal_eval(s):
+    try:
+        # Tries to convert the string into a list
+        return ast.literal_eval(s)
+    except (ValueError, SyntaxError):
+        # If an error occurs, returns a default value, e.g. an empty list
+        return []
+
+
+def check_nan_inf(df):
+    # Vérification des valeurs NaN
+    if df.isna().any().any():
+        print("There are NaN values in the dataframe")
+    else:
+        print("There are no NaN values in the dataframe")
+
+
+def remove_rows_with_nan(df):
+    return df.dropna()
+
+
+def signed_adjency(mat):
+    '''
+    L'idée est ici, à partir d'une matrice de corrélation mat, de renvoyer deux matrices 
+    A_positive et A_negative qui correspondraient aux matrices des corrélations positives et négatives 
+    associées  
+    '''
+
+    A_pos = mat.applymap(lambda x: x if x >= 0 else 0)
+    A_neg = mat.applymap(lambda x: abs(x) if x < 0 else 0)
+    
+    return A_pos, A_neg
+
+def apply_SPONGE(correlation_matrix, k): 
+
+    '''
+    IDÉE : étant donné une matrice de correlation obtenue à partir d'une base de donnée et de la similarité de pearson, renvoyer un vecteur associant 
+           à chaque actif le numéro du cluster auquel il appartient une fois qu'on lui a appliqué SPONGE (à partir du package signet)
+
+    PARAMS : 
+
+    - correlation_matrix : a square dataframe of size (number_of_stocks, number_of_stocks)
+    - k : the number of clusters to identify. If a list is given, the output is a corresponding list
+
+    RETURNS : array of int, or list of array of int: Output assignment to clusters.
+
+    '''
+    
+    ## On respecte le format imposé par signet. Pour cela il faut changer le type des matrices A_pos et A_neg, qui ne peuvent pas rester des dataframes 
+
+    A_pos, A_neg = signed_adjency(correlation_matrix)
+
+    A_pos_sparse = sparse.csc_matrix(A_pos.values)
+    A_neg_sparse = sparse.csc_matrix(A_neg.values)
+
+    data = (A_pos_sparse, A_neg_sparse)
+
+    cluster = Cluster(data)
+
+    ## On applique la méthode SPONGE : clusters the graph using the Signed Positive Over Negative Generalised Eigenproblem (SPONGE) clustering.
+
+    return cluster.SPONGE(k)
+
+
+def correlation_matrix(lookback_window, df_cleaned):
+
+
+    '''
+    ----------------------------------------------------------------
+    GENERAL IDEA : compute the correlation matrix of different stock 
+                   returns  over a given lookback_window
+    ----------------------------------------------------------------
+
+    ----------------------------------------------------------------
+    PARAMS : 
+    
+    - lookback_window : integer
+    
+    - df_cleaned : pandas dataframe containing the returns of the stocks
+
+    ----------------------------------------------------------------
+    '''
+ 
+    df = df_cleaned.iloc[:, :lookback_window+1] ## + 1 because we don't want to take into account the first column
+    df.set_index('ticker', inplace=True)
+    correlation_matrix = df.iloc[:, :31].corr(method='pearson')
+    return correlation_matrix
