1c6645d3b1d7d8690bb479cf5f388d5288fb1245
NailKhelifa <nail.khelifa@ensae.fr>
2024-03-18 21:24:28 +0100

    EMA_CV big modif

--- Code/EMA_CV.py
+++ Code/EMA_CV.py
@@ -43,16 +43,20 @@ class EMA_CV:
 
         ## 1. We extract the data corresponding to the returns of our assets (columns) during these d days (lines)
         X = self.historcal_data.iloc[self.lookback_window[0]:self.lookback_window[1],:] ## shape days * number of stocks
-
+        days = len(self.lookback_window)
         ## 2. We slightly adjust the matrix of observations to get the auxiliary matrix that puts more weight on recent dates
-        W = np.sqrt(np.diag(len(self.lookback_window) * (1 - self.beta) * self.beta**(np.arange(self.lookback_window[0], self.lookback_window[1])[::-1]) / (1 - self.beta**len(self.lookback_window))))  # Compute the weight matrix
-        X_tilde = pd.DataFrame(index=X.index, columns=X.columns, data=np.dot(W, X)).transpose()
+
+        # Compute the weight matrix : shape (days, days) (if days = 250, shape (250, 250))
+        W = np.sqrt(np.diag(days * (1 - self.beta) * self.beta**(np.arange(self.lookback_window[0], self.lookback_window[1])[::-1]) / (1 - self.beta**days)))  
+        X_tilde = pd.DataFrame(index=X.index, columns=X.columns, data=np.dot(W, X))
 
         ## 3. We randomize the auxiliary matrix of observations according to the time axis
         # Randomized_X = X_tilde.transpose().sample(frac=1, axis=1, random_state=42) ## we transpose X as we want to have daily observations of the whole dataset !
 
-        return X_tilde
+        return X_tilde ## shape (days, 695)
     
+
+
     ######################### 2. We then split the (randomized) auxiliary observations into K non-overlapping folds of equal size #########################
     def shuffle_split(self):
         # Initialize ShuffleSplit
@@ -63,53 +67,57 @@ class EMA_CV:
         splits = []
 
         # Perform shuffling and splitting
-        for train_index, test_index in shuffle_split.split(self.historcal_data.columns):
-            train_fold = [self.historcal_data.columns[i] for i in train_index]
-            test_fold = [self.historcal_data.columns[i] for i in test_index]
+        for train_index, test_index in shuffle_split.split(self.historcal_data.index):
+            train_fold = [self.historcal_data.index[i] for i in train_index]
+            test_fold = [self.historcal_data.index[i] for i in test_index]
             splits.append((train_fold, test_fold)) ## attention à cette structure
 
         return splits
     
     ######################### 3. For each K fold configuration, we estimate the sample eigenvectors from the training set #########################
-    def eigen_sample(self, train_fold): ## we train the data on this test fold
 
-        X_tilde_train = self.historcal_data.loc[:, train_fold]
+    def eigen_sample(self, train_fold):
+        ## 1. We extract the data corresponding to the returns of our assets (columns) during these d days (lines)
+        X = self.historcal_data.loc[train_fold] ## shape days * number of stocks
+        days = len(train_fold) 
 
-        # Calculer la moyenne de l'ensemble d'entraînement
-        mean_train = np.mean(X_tilde_train, axis=1)
+        # Compute the weight matrix : shape (days, days) (if days = 250, shape (250, 250))
+        W = np.sqrt(np.diag(days * (1 - self.beta) * self.beta**(np.arange(days)[::-1]) / (1 - self.beta**days)))  
 
-        # Centrer les données d'entraînement
-        centered_train_data = X_tilde_train.sub(mean_train, axis=0)
+        # We compute the auxiliary matrix
+        X_tilde = np.dot(W, X)
 
-        # Calculer la matrice de covariance des données d'entraînement
-        cov_matrix_train = np.dot(centered_train_data.T, centered_train_data) ## size number of assets * number of assets
+        # We compute the training sample exponential moving average 
+        sample_expo_cov = np.dot(X_tilde.T, X_tilde)
 
         # Calculer les vecteurs et valeurs propres de la matrice de covariance
-        _, eigenvectors_train = np.linalg.eigh(cov_matrix_train) ## .eigh and not .eig so that the eigenvalues are real 
+        _, eigenvectors_train = np.linalg.eigh(sample_expo_cov) ## .eigh and not .eig so that the eigenvalues are real 
 
         return eigenvectors_train
+    
+
     
     def intra_fold_loss(self, test_fold, sample_eigenvector_i): ## we test the data on this test fold
 
         ## 1. get the fold cardinality 
-        fold_cardinality = len(test_fold)
+        fold_cardinality = len(test_fold) ## 20% of the observations
 
         ## 2. sample vector of the auxiliary observation matrix from the test fold (inspired from the code above)
 
         days = len(test_fold)
-        X = self.historcal_data.loc[:,test_fold].transpose()
+        X = self.historcal_data.loc[test_fold] ## shape (days, 695)
 
         ## 2. We slightly adjust the matrix of observations to get the auxiliary matrix that puts more weight on recent dates
 
-        W = np.sqrt(np.diag(days * (1 - self.beta) * self.beta**(np.arange(days)[::-1]) / (1 - self.beta**days)))  # Compute the weight matrix
-        X_tilde = pd.DataFrame(index=X.index, columns=X.columns, data=np.dot(W, X)).transpose()
+        W = np.sqrt(np.diag(days * (1 - self.beta) * self.beta**(np.arange(days)[::-1]) / (1 - self.beta**days)))  # shape (days, days)
+        X_tilde = pd.DataFrame(index=X.index, columns=X.columns, data=np.dot(W, X)) # shape (days, 695)
 
-        res = (np.dot(sample_eigenvector_i, X_tilde) ** 2) / fold_cardinality
-        result = np.sum(res)
+        res = (np.dot(sample_eigenvector_i, X_tilde.T) ** 2)  # shape (, 695) * (695, days) = (, days)
+        result = np.sum(res) / fold_cardinality ## sum over days / size of the test sample
 
         return result
     
-    def average_loss_i(self, splits, index):
+    def average_loss(self, splits, index):
 
         res = 0 ## to stock the overall loss
 
@@ -126,46 +134,43 @@ class EMA_CV:
         res = res / len(splits) ## we average by the number of folds (which corresponds to the lengths of the splits)
 
         return res
+    
 
-    def eigenvalue_estimator(self):
+    def eigenvalue_estimator(self, splits):
 
-        data = self.auxilary_matrix()
-        
-        splits = self.shuffle_split()
-
-        number_of_stocks = data.shape[0]
+        number_of_stocks = len(self.historcal_data.columns) ## COLUMNS HAVE TO BE COMPOSED OF THE STOCKS TICKERS
 
         xi = np.zeros(number_of_stocks)  # initialisation de x
 
-        for index in tqdm(range(number_of_stocks), desc='Calcul en cours', unit='itération'):
-            xi[index] = self.average_loss_i(splits=splits, index=index)   
+        for i in tqdm(range(number_of_stocks), desc='Calcul en cours', unit='itération'):
+            xi[i] = self.average_loss(splits=splits, index=i)   
                         
         return xi
 
+
     def EMA_CV(self):
+
+        days = len(self.lookback_window)
         ## compute the sample exponential moving average correlation matrix
         X = self.historcal_data.iloc[self.lookback_window[0]:self.lookback_window[1],:]
-        W = np.diag(len(self.lookback_window) * (1 - self.beta) * self.beta**(np.arange(self.lookback_window[0], self.lookback_window[1])[::-1]) / (1 - self.beta**len(self.lookback_window)))  # Compute the weight matrix, no sqrt as we want the real matrix
-        res1 = np.dot(X.T, W)  # Produit matriciel de X' et W
-        S = np.dot(res1, X)
+        W = np.sqrt(np.diag(days * (1 - self.beta) * self.beta**(np.arange(self.lookback_window[0], self.lookback_window[1])[::-1]) / (1 - self.beta**days)))  
+        X_tilde = np.dot(W, X)  # Produit matriciel de X' et W
+        S = np.dot(X_tilde.T, X_tilde)
 
         ## compute the eigenvectors of S
-
         _, eigenvectors = np.linalg.eigh(S)
 
         ## computes the estimator 
-        eigenvalue_estimator = self.eigenvalue_estimator()
-
-        # Tailles des matrices
-        num_eigenvalues = eigenvalue_estimator.shape[0]
-        num_features = eigenvectors.shape[0]
+        X_tilde = self.auxilary_matrix()
+        splits = self.shuffle_split()
+        eigenvalue_est = self.eigenvalue_estimator(splits=splits)
 
         # Initialisation de Sigma avec des zéros
-        Sigma = np.zeros((num_features, num_features), dtype=np.double)
+        Sigma = np.zeros((S.shape[0], S.shape[1]), dtype=np.complex128)
 
         # Parcourir chaque vecteur propre et valeur propre
-        for i in range(num_eigenvalues):
-            xi_dagger = eigenvalue_estimator[i]  # Conjugue de xi
+        for i in range(len(self.historcal_data.columns)):
+            xi_dagger = eigenvalue_est[i]  # Conjugue de xi
             ui = eigenvectors[:, i]  # i-ème vecteur propre
 
             # Calcul du produit externe xi^† * ui * ui^† et addition à Sigma
