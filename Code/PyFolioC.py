6b4d4a6de64ba6a8f6c070fa7a8ecefc8fb5189d
jallier7 <jerome.allier@ensae.fr>
2024-04-02 14:01:13 +0200

    max sharpe

--- Code/PyFolioC.py
+++ Code/PyFolioC.py
@@ -593,7 +593,7 @@ class PyFolio:
         else: 
             ef = EfficientFrontier(expected_returns=expected_returns, cov_matrix=cov, weight_bounds=(0, 1))
 
-        ef.max_sharpe()
+        ef.max_sharpe(solver="SCS")
 
         markowitz_weights = ef.clean_weights()
 
