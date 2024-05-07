1077f558f510f95b5528710243a65d8f726b4ca7
jallier7 <jerome.allier@ensae.fr>
2024-05-07 12:10:13 +0200

    eta scaled

--- Code/PyFolioC.py
+++ Code/PyFolioC.py
@@ -645,10 +645,8 @@ class PyFolio:
                     # Ajout du bruit aux rendements de l'actif
                     noised_returns[asset] = asset_means[asset] + noise
 
-                x_min, x_max = asset_returns.min(), asset_returns.max()
-                y_min, y_max = noised_returns.min(), noised_returns.max()
-                # Mise à l'échelle de y pour qu'elle corresponde à l'échelle de x
-                y_scaled = (noised_returns - y_min) / (y_max - y_min) * (x_max - x_min) + x_min
+                # Mise à l'échelle de noised pour qu'elle corresponde à l'échelle des returns
+                y_scaled = noised_returns / (noise_std_dev*1000)
                 return y_scaled
     
 
